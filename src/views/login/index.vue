9ced8f75eb5d896ad7e8553f9cd1f88be2535944
yourname <youremail@example.com>
2019-08-19 22:03:41 +0800

    初始化项目-页面分析-新建登录和主页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="home">
-    home
+  <div>
+      登录页面
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'home',
-  components: {
 
-  }
 }
 </script>
+
+<style>
+
+</style>
