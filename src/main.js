1d6dadf624a236bf8589b4e8194b04721a1c5f5a
yourname <youremail@example.com>
2019-08-19 21:24:16 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less' // 引文件设置基础样式
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
