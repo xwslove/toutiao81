80244901d39ccc1b30f32b5ac54eae3b4dd611f2
yourname <youremail@example.com>
2019-08-22 19:27:16 +0800

    axios拦截器-统一处理响应数据

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,15 +1,23 @@
 import axios from 'axios'
-import { Promise } from 'q'
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 将后台接口的地址给axios的baseURL
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将后台接口的地址给axios的baseURL
+// 请求拦截器
 axios.interceptors.request.use(function (config) {
-  // 在发起请求做一些业务逻辑
-  // 进行配置注入
-  let userInfo = window.localStorage.getItem('user-info')// 获取用户存储信息 存储信息里有token
-  let token = userInfo ? JSON.parse(userInfo).token : null// 获取token
+  // 在发起请求请做一些业务处理
+  // 进行配置的注入
+  let userInfo = window.localStorage.getItem('user-info') // 获取用户存储信息 存储信息里有token
+  let token = userInfo ? JSON.parse(userInfo).token : null // 获取token
   config.headers.Authorization = `Bearer ${token}` // 直接将token注入到 请求参数中
   return config
 }, function (error) {
-  // 对请求失败处理
+  // 对请求失败做处理
+  return Promise.reject(error)
+})
+// 相应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做处理
+  return response.data ? response.data : {}
+}, function (error) {
+  // 对响应错误做处理
   return Promise.reject(error)
 })
 export default axios
