5b478e8793587f9c9d52202410c0542fae692615
yourname <youremail@example.com>
2019-08-22 19:12:39 +0800

    axios拦截器-统一处理请求token

--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import ElementUI from 'element-ui' // 引入UI
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import Component from './components'// 引入我们自定义的插件
 import './styles/index.less' // 引文件设置基础样式
-import axios from 'axios' // 引入axios
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+import axios from './utils/axios.config'// 引用自定义axios
 Vue.prototype.$axios = axios // 把axios插件给vue对象原型
 Vue.config.productionTip = false// 注册element组件
 Vue.use(ElementUI)// 注册element组建
