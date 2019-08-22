9f797f89150505e41dae75f7edca93b53409f215
yourname <youremail@example.com>
2019-08-22 19:55:52 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,12 @@
-import layoutAside from './home/layout-aside.vue'
-import layoutHeader from './home/layout-header.vue'
-
+import layoutAside from './home/layout-aside'
+import layoutHeader from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 // ElementUI导出的对象 对象里有一个办法
 // vue.use(elementUI)=>调用其提供的install反法
 export default {
   install (Vue) {
     Vue.component('layout-header', layoutHeader)// 注册layoutheader组建
     Vue.component('layout-aside', layoutAside)// 注册左侧组建
+    Vue.component('bread-crumb', breadCrumb)// 注册面包屑组件
   }
 }
