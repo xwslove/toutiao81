f192edb6f268f1419b9c001fa798ed53a0ecd1bc
yourname <youremail@example.com>
2019-08-25 20:11:09 +0800

    发布文章-注册富文本编辑器及应用

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,11 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
 import breadCrumb from './common/bread-crumb'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 // ElementUI导出的对象 对象里有一个办法
 // vue.use(elementUI)=>调用其提供的install反法
 export default {
@@ -8,5 +13,6 @@ export default {
     Vue.component('layout-header', layoutHeader)// 注册layoutheader组建
     Vue.component('layout-aside', layoutAside)// 注册左侧组建
     Vue.component('bread-crumb', breadCrumb)// 注册面包屑组件
+    Vue.component('qull-editor', quillEditor)// 注册quill组件成为全局对象
   }
 }
