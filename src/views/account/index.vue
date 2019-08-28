32d494f9e36efb87e59041595aab7b81d1abb7de
yourname <youremail@example.com>
2019-08-28 19:40:41 +0800

    账户信息-头像上传更新

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -1,31 +1,33 @@
 <template>
-  <el-card >
-      <bread-crumb slot='header'>
-       <template slot='title'>账户信息</template>
-      </bread-crumb>
-      <el-form ref="userForm" :model="formData" :rules='rules' label-width="100px">
-          <!-- 用户名 -->
-          <el-form-item prop="name" label='用户名称'>
-              <el-input v-model="formData.name" style='width:300px'></el-input>
-          </el-form-item>
-           <!-- 简介 -->
-          <el-form-item label='用户简介'>
-                <el-input v-model="formData.intro" style='width:300px'></el-input>
-          </el-form-item>
-          <!-- 邮箱 -->
-          <el-form-item prop="email" label='用户邮箱'>
-                <el-input v-model="formData.email" style='width:300px'></el-input>
-          </el-form-item>
-          <!-- 手机号 -->
-          <el-form-item label='用户手机号'>
-                <el-input v-model="formData.mobile" style='width:300px' disabled></el-input>
-          </el-form-item>
-         <!-- 保存按钮 -->
-         <el-form-item>
-             <el-button @clic="savUserInfo" type='primary'>保存信息</el-button>
-         </el-form-item>
-      </el-form>
-       <img class='head-img' :src="formData.photo ? formData.photo : imgUrl  " alt="">
+  <el-card v-loading="loading">
+    <bread-crumb slot="header">
+      <template slot="title">账户信息</template>
+    </bread-crumb>
+    <el-form ref="userForm" :model="formData" :rules="rules" label-width="100px">
+      <!-- 用户名 -->
+      <el-form-item prop="name" label="用户名称">
+        <el-input v-model="formData.name" style="width:300px"></el-input>
+      </el-form-item>
+      <!-- 简介 -->
+      <el-form-item label="用户简介">
+        <el-input v-model="formData.intro" style="width:300px"></el-input>
+      </el-form-item>
+      <!-- 邮箱 -->
+      <el-form-item prop="email" label="用户邮箱">
+        <el-input v-model="formData.email" style="width:300px"></el-input>
+      </el-form-item>
+      <!-- 手机号 -->
+      <el-form-item label="用户手机号">
+        <el-input v-model="formData.mobile" style="width:300px" disabled></el-input>
+      </el-form-item>
+      <!-- 保存按钮 -->
+      <el-form-item>
+        <el-button @click="saveUserInfo" type="primary">保存信息</el-button>
+      </el-form-item>
+    </el-form>
+      <el-upload class='head-img' action="" :show-file-list="false" :http-request="uploadImg">
+        <img  :src="formData.photo ? formData.photo : imgUrl  " alt="">
+      </el-upload>
   </el-card>
 </template>
 
@@ -33,6 +35,7 @@
 export default {
   data () {
     return {
+      loading: false,
       imgUrl: require('../../assets/img/404.png'),
       formData: {
         name: '',
@@ -43,14 +46,27 @@ export default {
       },
       rules: {
         name: [{ required: true, message: '用户名称不能为空' },
-          { min: 2, max: 10, message: '用户名称控制在5到10' }
-        ],
+          { min: 2, max: 10, message: '用户名称必须控制在2到10个字符' }],
         email: [{ required: true, message: '邮箱不能为空' },
           { pattern: /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/, message: '邮箱格式不正确' }]
       }
     }
   },
   methods: {
+    uploadImg (params) {
+      this.loading = true
+      let data = new FormData()
+      data.append('photo', params.file)
+      this.$axios({
+        method: 'patch',
+        url: '/user/photo',
+        data// 参数
+      }).then(result => {
+        // 获取到新的地址之后赋值给当前页面
+        this.formData.photo = result.data.photo
+        this.loading = false
+      })
+    },
     getUserInfo () {
       this.$axios({
         url: '/user/profile'
@@ -58,12 +74,12 @@ export default {
         this.formData = result.data
       })
     },
-    savUserInfo () {
+    saveUserInfo () {
       this.$refs.userForm.validate((isOK) => {
         if (isOK) {
           // 保存数据
           this.$axios({
-            method: 'path',
+            method: 'patch',
             url: '/user/profile',
             data: this.formData
           }).then(() => {
@@ -80,12 +96,14 @@ export default {
 </script>
 
 <style lang='less' scoped>
- .head-img {
-     position: absolute;
-     left: 700px;
-     top:150px;
-     height: 200px;
-     width: 200px;
-     border-radius: 50%;
+.head-img {
+  position: absolute;
+  left: 700px;
+  top: 150px;
+ img{
+  height: 200px;
+  width: 200px;
+  border-radius: 50%;
  }
+}
 </style>
