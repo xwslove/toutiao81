7e310979ca45e57747d04f1334fea92a9129b69a
yourname <youremail@example.com>
2019-08-28 21:00:27 +0800

    页面进度条引入应用

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,9 @@
 import router from './router'
+import nprogress from 'nprogress'// 引入文件
+import 'nprogress/nprogress.css'// 引入样式
 // 全局导航前置守卫
 router.beforeEach(function (to, from, next) {
+  nprogress.start()// 开始进度条
   // 如果是以home为起始就认为进入了需要检查oken的区域
   if (to.path.startsWith('/home')) {
     // 获取前端token
@@ -19,5 +22,9 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()// 直接放行
   }
+  // 如果没有进去home的领地 就不需要管了
+})
+router.afterEach(() => {
+  nprogress.done()
 })
 export default router
